f7fab2a9bf6badbda22158bbad3935b5bff2b202
PawelPohland <pawel.pohland@gmail.com>
2022-07-29 14:49:37 +0200

    Complete class to get page source (using requests module)

---
+++ scrapper_requests.py
@@ -0,0 +1,24 @@
+import requests
+from requests.exceptions import HTTPError
+
+class ScrapperRequests:
+
+    def __init__(self):
+        session = requests.Session()
+
+    def get_source_page(self, url):
+        page_source = None
+
+        try:
+            response = self.session.get(url)
+            response.raise_for_status()
+
+            page_source = response.text
+
+        except HTTPError as http_err:
+            print(f"HTTP error occured: {http_err}")
+            print(f"Response status code: {response.status_code}")
+        except Exception as err:
+            print(f"An error has occured: {err}")
+        finally:
+            return page_source
